43475ea0566d7130318a5640556b9b16f03bef7c
Xavier Corbeil <xcorbeil@hotmail.com>
2024-11-23 00:21:44 -0500

    iinit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { appWithTranslation } from "next-intl";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
